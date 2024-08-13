18f28b5a79b99bc8b9a0a6b3a47a29eec16b8096
wintun12 <winmin.tun@nhs.net>
2024-08-13 11:24:28 +0100

    inital commit for archer api upload dnanexus app

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
-# dnanexus_archer_api_upload
+# dnanexus_archer_api_upload_v1.0.0
+DNAnexus app to upload fastq files onto Archer and submit the job.
+
+## How the app works?
+Docker image "archer_api_upload" from https://github.com/moka-guys/archer_api_upload is used to run the app.
+
+## Input
+- DNAnexus project id where the fastq files are located (string)
+- Archer password in Base64 Encoded format (string)
+- Archer protocol id for the job (integer)
+
+## Output
+- logfile.txt - txt file containg the log for file uploading and job submission
+- sample_log.json - json file containing the list of samples submitted for the job. 
+In addition to the output files above, it is expected that the submitted job starts running on Archer job platform (https://analysis.archerdx.com/home#running_jobs)
+
+## Running from the CLI:
+
+The app can be run from the dx CLI. The example below shows the command line to be used:
+```
+dx run applet-xxx \
+-iproject_id=project-xxxx \
+-iarcher_password=<pw> \
+-iprotocol_id=<id>
+```
+
---
+++ src/code.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+set -e -x -o pipefail
+main() {
+# download all inputs
+mark-section "Download all inputs"
+dx-download-all-inputs --parallel
+
+# get project name and folder name
+project_name=$(dx describe $project_id --json | jq '.name')
+folder_name=$(echo $project_name | sed 's/_[^_]*$//g')
+folder_name=$(printf '%s\n' "${folder_name#*_}")
+
+# create output folder
+mkdir -p /home/dnanexus/out/archer_api_upload/archer_api_upload
+
+# Location of the archer_api_upload docker file
+docker_file_id=project-Gpp93y80b4Zk97Z3XpJPKz2j:file-Gpx212j0b4ZqY7xkvXyKy2bP
+
+# find fastq files in given project and folder
+fastq=$(dx find data --name "*.fastq.gz" --project "$project_id" --folder /${folder_name}/Data/Intensities/BaseCalls --brief)
+
+# donwload fastq files
+for fq in ${fastq[@]}; do
+    dx download $fq
+done
+
+# download docker image
+dx download $docker_file_id
+docker_file=$(dx describe ${docker_file_id} --name)
+DOCKERIMAGENAME=`tar xfO ${docker_file} manifest.json | sed -E 's/.*"RepoTags":\["?([^"]*)"?.*/\1/'`
+docker load < /home/dnanexus/"${docker_file}"
+echo $DOCKERIMAGENAME
+
+# run docker
+mark-section "Run archer_api_upload docker image"
+docker run -v /home/dnanexus/:/home/dnanexus/ $DOCKERIMAGENAME "/home/dnanexus" ${archer_password} ${folder_name} ${protocol_id} | tee -a "logfile.txt"
+
+# check if the file uploading and job submission are successful
+success=$(grep -o -P '(?<=success\":).*(?=,\"success)' logfile.txt |  sort | uniq)
+echo $success
+if [ "$success" != true ] ; then     
+    echo "Errors in file uploading or job submission. Check logfile.txt for more details"    
+    exit 1     
+fi
+
+# Upload output files
+mv "logfile.txt" /home/dnanexus/out/archer_api_upload/archer_api_upload/
+mv "sample_log.json" /home/dnanexus/out/archer_api_upload/archer_api_upload/
+dx-upload-all-outputs
+}
